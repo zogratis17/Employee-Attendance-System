976c7e678313a80bc27e76a9021cfab581172d11
HARI PRASATH N T <87246155+zogratis17@users.noreply.github.com>
2025-11-30 17:51:52 +0530

    Update configuration files
    - Modified Vite config for client build optimization
    - Updated environment variables template in server

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // Increase from default 500kb to 1000kb
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
+          'redux-vendor': ['@reduxjs/toolkit', 'react-redux'],
+          'chart-vendor': ['chart.js', 'react-chartjs-2'],
+        },
+      },
+    },
+  },
 })
